afc4ccb7230ab38251b8e97d672245c1d615e8fe
Faris <f.abualnaja15@gmail.com>
2024-07-27 17:23:24 +0100

    Created .gitignore file.

--- FDTD-1D.py
+++ FDTD-1D.py
@@ -7,7 +7,16 @@ import numpy
 import matplotlib
 
 # Parameters
+# Create the grid using n and j (100 x 100 grid)
+n = [i for i in range(1, 11)] # Temporal division
+j = [i for i in range(1, 11)] # Spatial division
 
+# Constants
+c = 3e8
+
+print(n)
+print(j)
+print(c)
 
 #Equations
 
