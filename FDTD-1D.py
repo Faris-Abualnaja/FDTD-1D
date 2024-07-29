52797ff5ec58a0f27f8cbe09533ec763741ee092
F. Abualnaja <fa465@cam.ac.uk>
2024-07-29 12:22:19 +0100

    Added simulation loop code
    Added the basic code for the simulation loop.

--- FDTD-1D.py
+++ FDTD-1D.py
@@ -8,10 +8,11 @@ import matplotlib.pyplot as plt
 
 # Define Constants
 # Constants
-u_0     = 1.25663706e-6         # Permeability of free space (magnetic constant)
+mu_0    = 1.25663706e-6         # Permeability of free space (magnetic constant)
 eps_0   = 8.85418782e-12        # Permitivitty of free space (electric constant)
-c_0     = 1/np.sqrt(u_0*eps_0)  # Speed of light in a vacuum (2.99792458e8)
-imp_0   = np.sqrt(u_0/eps_0)    # Impedance of free space
+c_0     = 1/np.sqrt(mu_0*eps_0)  # Speed of light in a vacuum (2.99792458e8)
+imp_0   = np.sqrt(mu_0/eps_0)    # Impedance of free space
+eps     = eps_0                 # Relative permitivitty
 
 # Size of simulation domain in space and time
 j_max = 500     # 500 cells
@@ -34,3 +35,21 @@ Hz_prev = np.zeros(j_max)
 
 
 # Simulation loop
+for n in range(n_max):
+    # Update magnetic field boundaries
+    Hz[j_max - 1] = Hz_prev[j_max - 2]
+    # Update magnetic field
+    for j in range(j_max-1):
+        Hz[j] = Hz_prev[j] + (dt/(mu_0*dy))*(Ex[j+1] - Ex[j])
+    
+    # Magnetic field source
+    # Hz[j_source -1] = Hz[j_source -1] + H0
+
+    # Update electric field boundaries
+    Ex[0] = Ex_prev[1]
+    # Update electric field
+    for j in range(1, j_max):
+        Ex[j] = Ex_prev[j] + (dt/(eps*dy))*(Hz[j] - Hz[j-1])
+
+    # Electric field source
+    # Ex[j_source] = Ex[j_source] + E0
