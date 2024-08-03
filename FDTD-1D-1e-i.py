0f3a2f0336b77e0ff7b950928123e53599c7cd5d
Faris <f.abualnaja15@gmail.com>
2024-08-03 19:23:43 +0100

    Sine wave source with absorbing boundaries

--- FDTD-1D-1e-i.py
+++ FDTD-1D-1e-i.py
@@ -12,15 +12,19 @@ from matplotlib.animation import PillowWriter
 # Define Constants
 
 # Size of simulation domain in space and time
-k_max       = 200 # 200 cells
-n_max       = 800 # 400 time stamps
-k_source    = 10   # Location of source in space (at the 1st cell)
+k_max    = 200 # 200 cells
+n_max    = 800 # 400 time stamps
+k_source = 5   # Location of source in space (at the 5th cell)
 
 # Constants
 mu_0    = 1.25663706e-6         # Permeability of free space (magnetic constant)
 eps_0   = 8.85418782e-12        # Permitivitty of free space (electric constant)
 c_0     = 1/np.sqrt(mu_0*eps_0) # Speed of light in a vacuum (2.99792458e8)
 eps_r   = 4                     # Relative permittivity
+
+# Spatial and temporal step sizes
+dy = 0.01       # Step size/cell size in space (y-direction)
+dt = dy/(2*c_0) # Step size in time
 
 # Dielectric constant accross material spatial-domain
 eps_material    = np.zeros(int(k_max/2))
@@ -47,11 +51,11 @@ Upper_Boundary = np.zeros(n_max) # Magnetic field propagating in the z-direction
 # Electric and magnetic field source
 def Source_Function(t):
     # Source parameters: t is an integer, the time step
-    spread  = 12       # Width (300 time steps) of Gaussian pulse
-    t_0     = spread*3 # Delay (offset of Gaussian pulse)
+    freq = 700e6
+    w_0  = 2*np.pi*freq
 
     # Function returns a Gaussian pulse
-    return 1*np.exp(-0.5 * ((t_0 - t) / spread) ** 2)
+    return np.sin(w_0*t*dt)
 
 # Setting up figure
 fig = plt.figure(figsize=(8,1.75))
@@ -61,7 +65,7 @@ metadata    = dict(title='FDTD-1D Simulation', artist='Faris-Abualnaja')
 writer      = PillowWriter(fps=15, metadata=metadata)
 
 # Simulation and animation creation loop
-with writer.saving(fig, 'Gifs/FDTD-1D-1d-iii.gif', 100):
+with writer.saving(fig, 'Gifs/FDTD-1D-1e-i.gif', 100):
     # Time loop
     for n in range(n_max):
         # Update electric field
@@ -86,7 +90,7 @@ with writer.saving(fig, 'Gifs/FDTD-1D-1d-iii.gif', 100):
             Hz[k] = Hz[k] + 0.5*(Ex[k] - Ex[k+1])
 
         # Plotting
-        if n % 10 == 0: # Frame rate
+        if n % 5 == 0: # Frame rate
             plt.rcParams['font.size'] = 12
             # Plot the E-field
             plt.plot(Ex, color='b', linewidth=1.5)
@@ -97,8 +101,8 @@ with writer.saving(fig, 'Gifs/FDTD-1D-1d-iii.gif', 100):
             plt.ylabel('E$_x$', fontsize='14')
             plt.xticks(np.arange(0, 201, step=20))
             plt.xlim(0, 200)
-            plt.yticks(np.arange(-0.7, 1.2, step=0.5))
-            plt.ylim(-0.7, 1.2)
+            plt.yticks(np.arange(-1.5, 1.2, step=0.5))
+            plt.ylim(-1.2, 1.4)
             plt.text(100, 0.5, 'T = {}'.format(n),
             horizontalalignment='center')
             plt.tight_layout()
